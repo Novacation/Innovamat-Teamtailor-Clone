90177a90f9fbc253ac0b3ec226e9a9ef4d48b260
Celestino <47398013+Novacation@users.noreply.github.com>
2023-03-22 09:29:04 -0300

    About our project section

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,14 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div
-    id="appContainer"
-    class="relative w-full overflow-y-auto overflow-x-hidden">
+  <div id="appContainer" class="relative w-full overflow-hidden">
     <RouterView />
   </div>
 </template>
 
 <style scoped>
 @import url('./assets/reset.css');
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300;400&display=swap');
 
 #appContainer {
   min-height: 100vh;
--- src/components/Start/MainSections/TheAboutOurProjectSection.vue
+++ src/components/Start/MainSections/TheAboutOurProjectSection.vue
@@ -1,11 +1,21 @@
 <template>
-  <section class="flex flex-col">
-    <h2 class="text-4xl w-[60%] text-center mx-auto font-bold text-neutral-800">
+  <section class="flex flex-col text-neutral-800 px-5 mb-4">
+    <h2 class="text-[40px] w-[80%]  text-center mx-auto font-bold leading-[50px]">
       About our project 🚀
     </h2>
+
+    <p class="text-full text-left mt-20 text-[20px] text-neutral-600 ">
+      Innovamat is a K-8 curriculum that democratizes the learning of
+      mathematics. We provide our students with the skills needed in the 21st
+      century by promoting meaningful learning and critical thinking. VC backed
+      and crushing it in the south of Europe and LaTam, our goal is to expand
+      and make math accessible in the western countries.
+    </p>
   </section>
 </template>
 
-
-<!-- Innovamat-Teamtailor-Clone
-My version of the Innovamat Teamtailor start page -->
+<style scoped>
+p {
+  font-family: 'Rubik', sans-serif;
+}
+</style>
