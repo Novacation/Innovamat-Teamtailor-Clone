8627702412b92ea31e73d0ef86c73ba5f9926200
Celestino <47398013+Novacation@users.noreply.github.com>
2023-03-22 12:56:45 -0300

    Adjustments to the workplace and culture section

--- src/components/Start/MainSections/TheWorkplaceAndCultureSection.vue
+++ src/components/Start/MainSections/TheWorkplaceAndCultureSection.vue
@@ -1,32 +1,44 @@
 <script setup></script>
 
 <template>
-  <section class="flex flex-col text-neutral-800 px-5 mb-4 py-20">
-    <h2
-      class="text-[40px] w-[80%] text-center mx-auto font-bold leading-[50px] drop-shadow-md ">
-      Workplace & culture
-    </h2>
+  <section class="flex flex-col text-neutral-800">
+    <div
+      class="w-full flex flex-col justify-center items-start max-w-[650px] mx-auto px-5 py-20">
+      <h2
+        class="text-[40px] w-[80%] text-center mx-auto font-bold leading-[50px] drop-shadow-md">
+        Workplace & culture
+      </h2>
 
-    <p class="w-full text-left text-[22px] mt-16 mb-4">
-      <strong>Our Purpose</strong>
-    </p>
+      <p class="w-full text-left text-[22px] mt-16 mb-4 text-neutral-700">
+        <strong>Our Purpose</strong>
+      </p>
 
-    <p class="text-full text-left text-[20px] text-neutral-600">
-      Unleash everyone’s potential through education.
-    </p>
+      <p class="text-full text-left text-[20px] text-neutral-600">
+        Unleash everyone’s potential through education.
+      </p>
 
-    <p class="w-full text-left text-[22px] mt-16 mb-4">
-      <strong>Our Purpose</strong>
-    </p>
+      <p class="w-full text-left text-[22px] mt-16 mb-4 text-neutral-700">
+        <strong>Our Vision</strong>
+      </p>
 
-    <p class="text-full text-left text-[20px] text-neutral-600">
-      Unleash everyone’s potential through education.
-    </p>
+      <p class="text-full text-left text-[20px] text-neutral-600">
+        To lead and become a reference for children and teachers in Math
+        education.
+      </p>
+    </div>
+
+    <div
+      id="workplaceImg"
+      class="w-full min-h-[400px] bg-cover bg-no-repeat bg-center"></div>
   </section>
 </template>
 
 <style scoped>
 section {
   background-color: rgb(232, 249, 248);
+}
+
+#workplaceImg {
+  background-image: url(https://images.teamtailor-cdn.com/images/s3/teamtailor-production/gallery_picture-v6/image_uploads/5981f6eb-11d8-452b-84d1-b73b24753316/original.jpeg);
 }
 </style>
